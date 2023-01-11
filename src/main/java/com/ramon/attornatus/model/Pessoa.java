6e7329fbd51eea8edb02d84bc3601fe9e0ee12aa
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-11 20:09:53 -0300

    Criada entidade Pessoa

---
+++ src/main/java/com/ramon/attornatus/model/Pessoa.java
@@ -0,0 +1,63 @@
+package com.ramon.attornatus.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="pessoa")
+public class Pessoa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String dataNascimento;
+
+    private String endereco;
+    
+    public Pessoa() {
+    }
+    
+    public Pessoa(Long id, String nome, String dataNascimento, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
