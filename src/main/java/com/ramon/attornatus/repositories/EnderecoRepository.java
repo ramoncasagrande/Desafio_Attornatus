f4a87aeb739cf7c7479ca36bbef0697eb4970f0c
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-11 21:50:52 -0300

    Refatoração dos Repositórios

--- src/main/java/com/ramon/attornatus/repositories/EnderecoRepository.java
+++ src/main/java/com/ramon/attornatus/repositories/EnderecoRepository.java
@@ -1,11 +1,10 @@
 package com.ramon.attornatus.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import com.ramon.attornatus.model.Endereco;
 
-@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
     
 }
--- src/main/java/com/ramon/attornatus/repositories/PessoaRepository.java
+++ src/main/java/com/ramon/attornatus/repositories/PessoaRepository.java
@@ -1,11 +1,10 @@
 package com.ramon.attornatus.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import com.ramon.attornatus.model.Pessoa;
 
-@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
     
 }
