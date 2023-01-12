9ae70090a6ef382e37cd82aca552273b95e3f4ff
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-12 20:45:06 -0300

    Criado EnderecoService + métodos

---
+++ src/main/java/com/ramon/attornatus/service/EnderecoService.java
@@ -0,0 +1,28 @@
+package com.ramon.attornatus.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramon.attornatus.model.Endereco;
+import com.ramon.attornatus.repositories.EnderecoRepository;
+
+@Service
+public class EnderecoService {
+    
+    @Autowired
+    EnderecoRepository enderecoRepository;
+
+    public Endereco adicionaEndereco(Endereco endereco){
+        return enderecoRepository.save(endereco);
+    }
+
+    public List<Endereco> ListarEnderecos(){
+        List<Endereco> lista = enderecoRepository.findAll();
+        return lista
+                .stream()
+                .collect(Collectors.toList());
+    }
+}
